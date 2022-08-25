5448c3594a748025866321ed4fde8370518763f7
cipengx <109833966+cipengx@users.noreply.github.com>
2022-08-25 19:44:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // INI SC MASIH ADA BUG YA FIX IN AJA
 // BASE ILMANHDYT
-// SC ORI HYZER
+// SC ORI HYZER.   
 // CREATOR KRIZYN OFC
 // RECODE BY OSCAR OFC
 
@@ -42,7 +42,154 @@ global.fotonya6 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ini gant
 global.fotonya7 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg'
 global.fotonya8 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //foto session
 global.donasi = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ganti pakai poto payment atau apa
+global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v// INI SC MASIH ADA BUG YA FIX IN AJA
+// BASE ILMANHDYT
+// SC ORI HYZER
+// CREATOR KRIZYN OFC
+// RECODE BY OSCAR OFC
+
+//=============『 Utama 』================== //
+global.owner = ['6281585328096','6285716583868'] 
+global.mods = [''] 
+global.prems = ['6285850539404'] 
+
+//=============『 Info Owner 』============== //
+global.nameowner = 'ᴹᴿ᭄DitNgtod꧂'
+global.numberowner = '6281585328096','6285716583868',
+global.instagram = 'https://instagram.com/Jarotr_'
+global.github = 'https://github.com/Alpiii22'
+global.Youtube = 'https://youtube.com/channel/UC1QtH-ZWtzzkDh6tsj7nLUQ'
+global.sc = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+global.ss = 'https://youtu.be/3m7tRD4RklM'
+global.sss = 'https://youtu.be/PzUH1ApMCig'
+global.yt = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
+global.dana = '085850539404'
+global.pulsa = '085716583868'
+global.gopay = '085850539404'
+
+//=============『 Info Bot 』=================//
+global.namebot = '❋ཻུ۪۪⸙PENGX BOT⳹ ❋ཻུ۪۪'
+global.gc = 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+global.web = 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.webm = 'https://instagram.com/Jarotr_'
+global.pulsa = '085850539404'
+global.nomer = 'https://wa.me/6285850539404' //ganti jadi nomor mu
+global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERBULAN Rp 15.000\nMAKSIMAL 2 GRUP\n》2 BULAN 25.000\nMAKSIMAL 3 GRUP \n》3 BULAN Rp 40.000\nMAKSIMAL 2 GRUP'
+
+//=======『 Tampilan Dan Lainnya 』============//
+global.fotonya1 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ini juga ganti
+global.fotonya3 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg'
+global.fotonya8 = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //foto session
+global.donasi = 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
+global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
+global.wm = '❋ཻུ۪۪⸙Dark-MD⳹ ❋ཻུ۪۪'
+global.watermark = wm
+global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜ𝐉𝐚𝐫𝐨𝐭_𝙊𝙛𝙛cོ1꧅'
+global.wm3 = 'ᯤ 𝐉𝐚𝐫𝐨𝐭 𝙾𝙵𝙲'
+global.htki = '––––––『' 
+global.htka = '』––––––'
+global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.wait = '⏳Tunggu kurang lebih 1 menit karna sedang diproses. . .'
+global.eror = '_*🚨 Server Bot Sedang Error*_'
+global.benar = 'Benar ✅\n'
+global.salah = 'Salah ❌\n'
+global.stiker_wait = '⏳ Stiker sedang Proses'
+global.packname = 'DitNgtod'
+global.author = 'GksukaNgocox'
+
+//=============『 Apikey 』================== //
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  amel: 'https://melcanz.com',
+    bx: 'https://bx-hunter.herokuapp.com',
+  dhnjing: 'https://dhnjing.xyz',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  nzcha: 'http://nzcha-apii.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  fdci: 'https://api.fdci.se',
+  dzx: 'https://api.dhamzxploit.my.id',
+  bsbt: 'https://bsbt-api-rest.herokuapp.com',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  zekais: 'http://zekais-api.herokuapp.com',
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz', 
+  erdwepe: 'https://erdwpe-api.herokuapp.com',
+  lolhuman: 'https://api.lolhuman.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://melcanz.com': 'elaina',
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://api.xteam.xyz': 'd37372311698ed1d',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://pencarikode.xyz': 'pais', 
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://zekais-api.herokuapp.com': 'apikeymu',
+  'https://api.lolhuman.xyz': 'Deffbotz',
+}
+
+//=============『 RPG GAMES 』================== //
+global.multiplier = 69 // The higher, The harder levelup
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      petFood: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+//===========『 Jangan Di Ubah 』================ //
+let fs = require('fs')
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
+
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = '❋ཻུ۪۪⸙DitNgtod❋ཻུ۪۪'
 global.watermark = wm
